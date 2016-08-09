fe0e3f007fb42a0904401966d4d1d33b4d1abefb
peter <peter@peters-Mac-Pro.local>
2016-08-08 19:28:37 -0700

    Name replace tentatively working

--- HW05_ex00_TextAdventure.py
+++ HW05_ex00_TextAdventure.py
@@ -6,12 +6,14 @@ from sys import exit
 
 # Body
 
+def name_replace(text):
+    return print(text.replace('You' or 'you',username))
 
 def infinite_stairway_room(count=0):
     print("You walk through the door to see a dimly lit hallway.")
-    print("At the end of the hallway is a", count * 'long ', 'staircase going towards some light')
+    print("At the end of the hallway is a", count ,'long ', 'staircase going towards some light')
     next = input("> ")
-    
+
     # infinite stairs option
     if next == "take stairs":
         print('You take the stairs')
@@ -19,8 +21,11 @@ def infinite_stairway_room(count=0):
             print("but you're not happy about it")
         infinite_stairway_room(count + 1)
     # option 2 == ?????
-    if next == option_2:
-        pass
+    if count>5 and next == 'mercy':
+        back_room()
+
+    else:
+        dead("You starve on the endless stairs.")
 
 
 def gold_room():
@@ -46,20 +51,30 @@ def bear_room():
     print("How are you going to move the bear?")
     bear_moved = False
 
+    #TODO: allow take or honey, open or door, and just taunt
     while True:
         next = input("> ")
 
-        if next == "take honey":
-            dead("The bear looks at you then slaps your face off.")
-        elif next == "taunt bear" and not bear_moved:
-            print("The bear has moved from the door. You can go through it now.")
-            bear_moved = True
-        elif next == "taunt bear" and bear_moved:
-            dead("The bear gets pissed off and chews your leg off.")
-        elif next == "open door" and bear_moved:
-            gold_room()
+        if bear_moved == True:
+            if next.find("taunt") != -1 or next.find("bear") != -1:
+                dead("The bear gets pissed off and chews your leg off.")
+
+            elif next.find("open") != -1 or next.find("door") != -1:
+                gold_room()
+
+            else:
+                print("I got no idea what that means.")
         else:
-            print("I got no idea what that means.")
+
+            if next.find('take') != -1 or next.find('honey') != -1:
+                dead("The bear looks at you then slaps your face off.")
+
+            elif next.find("taunt") != -1 or next.find("bear") != -1 :
+                print("The bear has moved from the door. You can go through it now.")
+                bear_moved = True
+
+            else:
+                print("I got no idea what that means.")
 
 
 def cthulhu_room():
@@ -70,12 +85,21 @@ def cthulhu_room():
     next = input("> ")
 
     if "flee" in next:
-        start()
+        infinite_stairway_room()
     elif "head" in next:
         dead("Well that was tasty!")
     else:
         cthulhu_room()
 
+def back_room():
+    print("You stumble into a dark room filled people huddled around computers.")
+    print("The sound of typing on keyboards slowly recedes. They stare at you.")
+    print("You aren't sure what to do in the silence, so you decide to announce yourself.")
+    print("You say:")
+    next = input("> ")
+    print("The crowd says all at once'", next ,"'and then returns to typing.")
+    print("You sit in a free chair and begin programming, forever.")
+    exit(0)
 
 def dead(why):
     print("{}\n Good job!".format(why))
@@ -83,9 +107,13 @@ def dead(why):
 
 
 ############################################################################
-def start():
+def main():
     # START the TextAdventure game
-    print("You are in a dark room.")
+    print("Please enter your name.")
+    global username
+    username = input("> ")
+
+    name_replace("You are in a dark room.")
     print("There is a door to your right and left.")
     print("Which one do you take?")
 
@@ -99,4 +127,4 @@ def start():
         dead("You stumble around the room until you starve.")
 
 if __name__ == '__main__':
-    start()
+    main()
